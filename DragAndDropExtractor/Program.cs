abb710dd4f2db9b82f05c0fde7a64043adb755b8
smatthew <sandor.mate@stud.uni-obuda.hu>
2023-10-29 08:30:30 +0100

    Add project files.

---
+++ DragAndDropExtractor/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            string filePath = args[0];
+            Console.WriteLine($"You dropped the file: {filePath}");
+        }
+        else
+        {
+            Console.WriteLine("Please drag and drop a file onto the executable.");
+        }
+    }
+}
