73e558494fe45c861b78c02291d827b51a7d5393
Máté Sándor <60856833+sndrmate@users.noreply.github.com>
2023-10-29 10:23:55 +0100

    base functions

--- DragAndDropExtractor/Program.cs
+++ DragAndDropExtractor/Program.cs
@@ -2,21 +2,83 @@
 using System;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.Title = "DragAndDropExtractor V1.0 Beta | by smatthew";
+        var DefaultColor = Console.ForegroundColor;
+        string extractPath = Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+ "\\Virtuali\\MSFS\\GSX");
         if (args.Length > 0)
         {
-            string filePath = args[0];
-            Console.WriteLine($"You dropped the file: {filePath}");
-            string extractPath = @".\\extract";
-            ZipFile.ExtractToDirectory(filePath, extractPath);
+            try
+            {
+                string ZipPath = args[0];
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"You dropped the file: {ZipPath}");
+
+                if (!extractPath.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+                    extractPath += Path.DirectorySeparatorChar;
+
+                using (ZipArchive archive = ZipFile.OpenRead(ZipPath))
+                {
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        string icao_code = entry.FullName.Split('-')[0];
+                        string[] matchingFiles = Directory.GetFiles(extractPath, $"*{icao_code}*");
+
+                        if (matchingFiles.Any())
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nFiles overwritten:");
+                            Console.ForegroundColor = DefaultColor;
+                            foreach (string file in matchingFiles)
+                            {
+                                Console.WriteLine(file);
+                                File.Delete(file);
+                            }
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nExtracted files:");
+                    Console.ForegroundColor = DefaultColor;
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.FullName.EndsWith(".py", StringComparison.OrdinalIgnoreCase) || entry.FullName.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
+                        {   
+                            // Gets the full path to ensure that relative segments are removed.
+                            string destinationPath = Path.GetFullPath(Path.Combine(extractPath, entry.FullName));
+
+                            // Ordinal match is safest, case-sensitive volumes can be mounted within volumes that
+                            // are case-insensitive.
+                            if (destinationPath.StartsWith(extractPath, StringComparison.Ordinal))
+                            {
+                                Console.WriteLine(destinationPath);
+                                entry.ExtractToFile(destinationPath);
+                            }
+                                
+                        }
+                    }
+                }
+                Console.WriteLine("\nPress any key to exit.");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+            }
+            
         }
         else
         {
-            Console.WriteLine("Please drag and drop a file onto the executable.");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Please drag and drop a file onto the DragAndDropExtractor.exe.");
+            Console.ForegroundColor = DefaultColor;
+            Console.WriteLine("\nPress any key to exit.");
         }
         Console.ReadKey();
     }
