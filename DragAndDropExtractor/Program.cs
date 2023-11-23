3831123dc3637c4d3775e23eab39b31450bb0002
sndrmate <60856833+sndrmate@users.noreply.github.com>
2023-11-23 20:21:43 +0100

    added namespace

--- DragAndDropExtractor/Program.cs
+++ DragAndDropExtractor/Program.cs
@@ -4,91 +4,95 @@
  */
 using SharpCompress;
 using SharpCompress.Archives;
-class Program
+
+namespace DragAndDropExtractor
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Title = "GSX Pro Profile Installer-dev v1.0";
-        ConsoleColor DefaultColor = Console.ForegroundColor;
-        string extractPath = Path.Combine(Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)), "Virtuali", "GSX", "MSFS");
-
-        
-        if (args.Length == 0)
-        // IDEA: We should make a search for .py and .ini files in the running directory (or copy the archive to the terminal?).
+        static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Please drag and drop a file onto the executable.");
-            Console.ForegroundColor = DefaultColor;
-            Console.WriteLine("\nPress any key to exit.");
-            Console.ReadKey();
-            return;
-        }
+            Console.Title = "GSX Pro Profile Installer-dev v1.0";
+            ConsoleColor DefaultColor = Console.ForegroundColor;
+            string extractPath = Path.Combine(Path.GetFullPath(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)), "Virtuali", "GSX", "MSFS");
 
-        try
-        {
-            string zipPath = args[0];
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"You initiated the installation process from this archive:\n{zipPath}");
-            Console.ForegroundColor = DefaultColor;
-            List<string> overwrittenFiles = new();
-            List<string> extractedFiles = new();
 
-            using (var archive = ArchiveFactory.Open(zipPath))
+            if (args.Length == 0)
+            // IDEA: We should make a search for .py and .ini files in the running directory (or copy the archive to the terminal?).
             {
-                /* Problem: Double foreach not so efficient. Maybe we should iterate through just once the archive and save the .py and .ini file names, then
-                overwrite files with the same icao code and then just extract the saved files from the archive? It would need only 1 foreach and we could use the already existing list. */
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please drag and drop a file onto the executable.");
+                Console.ForegroundColor = DefaultColor;
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+                return;
+            }
 
-                // Feature: If there is no .py or .ini files in the root directory of the archive, then check if there is a folder and search in that.
+            try
+            {
+                string zipPath = args[0];
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"You initiated the installation process from this archive:\n{zipPath}");
+                Console.ForegroundColor = DefaultColor;
+                List<string> overwrittenFiles = new();
+                List<string> extractedFiles = new();
 
-                foreach (IArchiveEntry entry in archive.Entries)
+                using (var archive = ArchiveFactory.Open(zipPath))
                 {
-                    string icao_code = entry.Key.Split('-')[0];
-                    string[] matchingFiles = Directory.GetFiles(extractPath, $"*{icao_code}*");
-                    overwrittenFiles.AddRange(matchingFiles);
-                    matchingFiles.ForEach(filePath => File.Delete(filePath));            
-                }
-                foreach (IArchiveEntry entry in archive.Entries)
-                {
-                    if (entry.Key.EndsWith(".py", StringComparison.OrdinalIgnoreCase)
-                        || entry.Key.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
+                    /* Problem: Double foreach not so efficient. Maybe we should iterate through just once the archive and save the .py and .ini file names, then
+                    overwrite files with the same icao code and then just extract the saved files from the archive? It would need only 1 foreach and we could use the already existing list. */
+
+                    // Feature: If there is no .py or .ini files in the root directory of the archive, then check if there is a folder and search in that.
+
+                    foreach (IArchiveEntry entry in archive.Entries)
                     {
-                        string finalExtractPath = Path.GetFullPath(Path.Combine(extractPath, entry.Key));
-                        extractedFiles.Add(finalExtractPath);
-                        using (Stream stream = entry.OpenEntryStream())
+                        string icao_code = entry.Key.Split('-')[0];
+                        string[] matchingFiles = Directory.GetFiles(extractPath, $"*{icao_code}*");
+                        overwrittenFiles.AddRange(matchingFiles);
+                        matchingFiles.ForEach(filePath => File.Delete(filePath));
+                    }
+                    foreach (IArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.Key.EndsWith(".py", StringComparison.OrdinalIgnoreCase)
+                            || entry.Key.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
                         {
-                            using (FileStream writer = File.OpenWrite(finalExtractPath))
+                            string finalExtractPath = Path.GetFullPath(Path.Combine(extractPath, entry.Key));
+                            extractedFiles.Add(finalExtractPath);
+                            using (Stream stream = entry.OpenEntryStream())
                             {
-                                stream.CopyTo(writer);
+                                using (FileStream writer = File.OpenWrite(finalExtractPath))
+                                {
+                                    stream.CopyTo(writer);
+                                }
+
                             }
+                        }
+                        else if (entry.IsDirectory)
+                        {
+                            string searchPath = Path.GetFullPath(Path.Combine(zipPath, entry.Key));
 
                         }
                     }
-                    else if (entry.IsDirectory)
-                    {
-                        string searchPath = Path.GetFullPath(Path.Combine(zipPath, entry.Key));
-
-                    }
                 }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nFiles overwritten:\n" + string.Join('\n', overwrittenFiles));
+                Console.ForegroundColor = DefaultColor;
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nExtracted files:\n" + string.Join('\n', extractedFiles));
+                Console.ForegroundColor = DefaultColor;
+                Console.WriteLine();
             }
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("\nFiles overwritten:\n" + string.Join('\n', overwrittenFiles));
-            Console.ForegroundColor = DefaultColor;
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("\nExtracted files:\n" + string.Join('\n', extractedFiles));
-            Console.ForegroundColor = DefaultColor;
-            Console.WriteLine();
-        }
-        catch (Exception e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(e.ToString());
-        }
-        finally
-        {
-            Console.ForegroundColor = DefaultColor;
-            Console.WriteLine("\nPress any key to exit.");
-            Console.ReadKey();
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.ForegroundColor = DefaultColor;
+                Console.WriteLine("\nPress any key to exit.");
+                Console.ReadKey();
+            }
         }
     }
 }
