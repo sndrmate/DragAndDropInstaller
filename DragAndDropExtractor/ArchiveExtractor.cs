939121e9c440af171b7bd095191e96eb3895449d
sndrmate <60856833+sndrmate@users.noreply.github.com>
2023-12-05 20:56:35 +0100

    variable renamings, multiple profile support

--- DragAndDropExtractor/ArchiveExtractor.cs
+++ DragAndDropExtractor/ArchiveExtractor.cs
@@ -11,11 +11,12 @@ namespace DragAndDropInstaller;
 class ArchiveExtractor
 {
     string destinationPath;
-    List<IArchiveEntry> extractQueue = new List<IArchiveEntry>();
-    List<IArchiveEntry> installQueue1 = new List<IArchiveEntry>();
-    List<IArchiveEntry> installQueue2 = new List<IArchiveEntry>();
+    List<IArchiveEntry> toExtract = new List<IArchiveEntry>();
+    List<IArchiveEntry> DotPyFiles = new List<IArchiveEntry>();
+    List<IArchiveEntry> DotIniFiles = new List<IArchiveEntry>();
     List<string> deletedFiles = new List<string>();
     List<string> installedFiles = new List<string>();
+    ConsoleColor Default = Console.ForegroundColor;
 
     public ArchiveExtractor(string destinationPath)
     {
@@ -29,20 +30,47 @@ class ArchiveExtractor
         Console.ForegroundColor = ConsoleColor.Yellow; //remove this line before merge to master
         foreach (IArchiveEntry entry in archive.Entries)
         {
-            if (IsSupportedFile(entry.Key))
+            if (IsDotIni(entry.Key))
             {
-                Console.WriteLine($"DEBUG: FILE FOUND AND PROCESSING BEGINS: {entry.Key}"); //remove this line before merge to master
-                extractQueue.Add(entry);
+                Console.WriteLine($"DEBUG: FILE FOUND AND PROCESSING BEGINS: {entry.Key}\n"); //remove this line before merge to master
+                DotIniFiles.Add(entry);
+                string[] matchingFiles = Directory.GetFiles(destinationPath, $"*{GetICAOcode(entry.Key)}*");
+                deletedFiles.AddRange(matchingFiles);
+                matchingFiles.ForEach(filePath => File.Delete(filePath));
+            }
+            else if (IsDotPy(entry.Key))
+            {
+                Console.WriteLine($"DEBUG: FILE FOUND AND PROCESSING BEGINS: {entry.Key}\n"); //remove this line before merge to master
+                DotPyFiles.Add(entry);
                 string[] matchingFiles = Directory.GetFiles(destinationPath, $"*{GetICAOcode(entry.Key)}*");
                 deletedFiles.AddRange(matchingFiles);
                 matchingFiles.ForEach(filePath => File.Delete(filePath));
             }
         }
-        if (extractQueue?.Any() == false)
+        if (DotIniFiles?.Any() == false && DotPyFiles?.Any() == false)
         {
             throw new Exception("ERROR: The archive is empty or no relevant files have been found.\n"); //Need to improve error handling
         }
-        foreach (IArchiveEntry entry in extractQueue)
+        if (DotIniFiles.Count >= 2 && DotPyFiles.Count == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("ATTENTION! Multiple profiles detected!\nPlease choose one.");
+            HandleMultipleProfiles(DotIniFiles);
+            toExtract.AddRange(DotPyFiles);
+        }
+        else if (DotIniFiles.Count >= 2 && DotPyFiles.Count >= 2)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("ATTENTION! Multiple profiles detected!\nPlease choose one.");
+            HandleMultipleProfiles(DotIniFiles);
+            HandleMultipleProfiles(DotPyFiles);
+        }
+        else 
+        {
+            toExtract.AddRange(DotIniFiles);
+            toExtract.AddRange(DotPyFiles);
+        }
+        foreach (IArchiveEntry entry in toExtract)
         {
             string filename = entry.Key;
             if (entry.Key.Contains('/')) { filename = entry.Key.Split('/').Last(); }
@@ -53,12 +81,29 @@ class ArchiveExtractor
             stream.CopyTo(writer);
         }
     }
-    bool IsSupportedFile(string fileName)
+    bool IsDotIni(string fileName)
     {
-        if (fileName.EndsWith(".py", StringComparison.OrdinalIgnoreCase)
-                    || fileName.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
+        if (fileName.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
         { return true; }
         return false;
+    }
+    bool IsDotPy(string fileName)
+    {
+        if (fileName.EndsWith(".py", StringComparison.OrdinalIgnoreCase))
+        { return true; }
+        return false;
+    }
+    void HandleMultipleProfiles(List<IArchiveEntry> list)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine($"\n{list.First().Key.Split('.').Last().ToUpper()} files:");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine($"[{i}] {list[i].Key}");
+        }
+        Console.Write("Choice: ");
+        Console.ForegroundColor = Default;
+        toExtract.Add(list[(int.Parse(Console.ReadLine()))]);
     }
     public string DisplayInstalledFiles()
     {
